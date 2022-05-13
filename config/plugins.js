38a3e925272a4341bb673d391cebbd02f40ac28d
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-05-13 14:49:06 +0200

    Configure graphql

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,15 @@
 module.exports = ({ env }) => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 100,
+      apolloServer: {
+          tracing: false,
+        },
+      },
+  },
   slugify: {
     enabled: true,
     config: {
