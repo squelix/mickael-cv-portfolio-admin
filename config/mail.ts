98dcdd74bc0d3f0887489615a4df8eeb30aef661
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-08-14 17:36:16 +0200

    Add re-captcha verification to email sender

--- config/mail.ts
+++ config/mail.ts
@@ -1,4 +1,5 @@
 export default ({ env }) => ({
   sender: env('SENDGRID_SENDER'),
   accessToken: env('MAIL_ACCESS_TOKEN'),
+  reCaptchaSecretKey: env('RE_CAPTCHA_PRIVATE_KEY'),
 });
--- src/api/mail/controllers/mail.ts
+++ src/api/mail/controllers/mail.ts
@@ -1,24 +1,42 @@
+import axios from 'axios';
+import url from 'url';
+
 export default {
   async send(ctx) {
     try {
+      const { token, subject, text, html } = ctx.request.body;
       const accessToken = ctx.request.headers['x-access-token'];
       const myAccessToken = strapi.config.get('mail.accessToken');
 
-      if (!!accessToken && accessToken === myAccessToken) {
-        const { subject, text, html } = ctx.request.body;
-
-        await strapi.plugins['email'].services.email.send({
-          to: strapi.config.get('mail.sender'),
-          from: strapi.config.get('mail.sender'),
-          replyTo: strapi.config.get('mail.sender'),
-          subject,
-          text,
-          html,
-        });
-        ctx.status = 204;
-      } else {
+      if (!token || !subject || !text || !html) {
+        ctx.status = 400;
+        ctx.body = 'Bad Request';
+      } else if (!accessToken || accessToken !== myAccessToken) {
         ctx.status = 401;
         ctx.body = 'Unauthorized';
+      } else {
+        const params = new url.URLSearchParams({
+          secret: strapi.config.get('mail.reCaptchaSecretKey'),
+          response: token,
+        });
+        const { success } = await axios
+          .post('https://www.google.com/recaptcha/api/siteverify', params.toString())
+          .then((response) => response.data);
+
+        if (!success) {
+          ctx.status = 422;
+          ctx.body = 'Captcha not good';
+        } else {
+          await strapi.plugins['email'].services.email.send({
+            to: strapi.config.get('mail.sender'),
+            from: strapi.config.get('mail.sender'),
+            replyTo: strapi.config.get('mail.sender'),
+            subject,
+            text,
+            html,
+          });
+          ctx.status = 204;
+        }
       }
     } catch (err) {
       ctx.body = err;
