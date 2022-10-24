2db86840153cb62bead6fdd700949470dc8f407a
Mickaël Depardon <mickael.depardon@wizbii.com>
2022-10-24 09:20:59 +0200

    Remove unused models

--- src/api/icon/controllers/icon.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  icon controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::icon.icon');
--- src/api/icon/services/icon.ts
+++
@@ -1,7 +0,0 @@
-/**
- * icon service.
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::icon.icon');
